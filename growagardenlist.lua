eefbd4a659dfeb56a3863244955b797092aee27e
KemilingHUB <168331164+KemilingHUB@users.noreply.github.com>
2025-05-23 17:45:11 +0700

    Create growagardenlist.lua

---
+++ growagardenlist.lua
@@ -0,0 +1,37 @@
+-- Admin KemilingHUB
+Usn: Henclin | Key: 1 | Exp: 01/01/3000
+Usn: KemilingHUB2 | Key: 1111 | Exp: 01/01/3000
+Usn: Janggaliwir | Key: 1111 | Exp: 01/01/3000
+Usn: Pengemudisumatra | Key: 1111 | Exp: 01/01/3000
+
+-- Free (Premium) user
+Usn: DanzzRBX | Key: zAkSaxmz | Exp: 01/01/3000
+
+-- Buyer Ingame
+Usn: sevshtz | Key: jxXepQ5XR | Exp: 1/04/2025 12:57
+
+-- Nbuy User
+Usn: Henclin | Nbuy: true
+Usn: DanzzRBX | Nbuy: true
+
+-- Note User
+Usn: ATHONKGAMING | Key: ucNXxhceLy | Note: JAWA JAWA JAWA
+
+-- Blacklist
+Usn: venzrazel | Blacklist: true
+
+-- Blacklist (Admin Ingame)
+Usn: niohailhad | Blacklist: true
+Usn: Emceeee0 | Blacklist: true
+Usn: dizon_3490 | Blacklist: true
+Usn: vj101yt | Blacklist: true
+Usn: MrDash_68 | Blacklist: true
+Usn: IInsaneStorage | Blacklist: true
+Usn: yuhanforyoupls | Blacklist: true
+Usn: kyrziis | Blacklist: true
+Usn: KingSnoopJay | Blacklist: true
+Usn: Nel_cookie | Blacklist: true
+Usn: todoroki_kudo | Blacklist: true
+Usn: DuminuStudio | Blacklist: true
+Usn: todoroki_kudo | Blacklist: true
+Usn: xxrosero09 | Blacklist: true
